3e5b12b40635a8479dc04a937f4724b0c8b46a50
Akihiro Nagami <nagami@kikyos.co.jp>
2019-09-01 12:28:13 +0900

    子要素取得

---
+++ script.js
@@ -0,0 +1,12 @@
+$(function() {
+  $('#find-method').click(function() {
+    $('#wrapper').find('a').css('color','red');
+    
+    
+  });
+  
+  $('#children-method').click(function() {
+    $('#wrapper').children('a').fadeOut();
+    
+  });
+});
